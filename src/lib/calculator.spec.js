3534e3d36f178b4c5344676bc0262b220b2849ea
AndySS <andy_ss_22@icloud.com>
2021-12-09 15:32:52 -0300

    done more math operations

--- src/lib/calculator.spec.js
+++ src/lib/calculator.spec.js
@@ -1,27 +1,94 @@
-const { sum } = require('./calculator');
+import { sum } from './calculator';
+import { sub } from './calculator';
+import { mult } from './calculator';
+import { div } from './calculator';
 
-it('should sum 2 and 2 and the result must be 4', () => {
-    expect(sum(2,2)).toBe(4);
-});
+describe('Sum between two numbers', () => {
+  it('should sum 2 and 2 and the result must be 4', () => {
+    expect(sum(2, 2)).toBe(4);
+  });
 
-it('should sum 2 and 2 even if one of them is a string and the result must be 4', () => {
+  it('should sum 2 and 2 even if one of them is a string and the result must be 4', () => {
     expect(sum('2', '2')).toBe(4);
+  });
+
+  it('should throw an error if what is provided to the method cannot be summed', () => {
+    expect(() => {
+      sum('', 2);
+    }).toThrowError();
+
+    expect(() => {
+      sum([2, 2]);
+    }).toThrowError();
+
+    expect(() => {
+      sum({});
+    }).toThrowError();
+
+    expect(() => {
+      sum();
+    }).toThrowError();
+  });
 });
 
-it('should throw an error if what is provided to the method cannot be summed', () => {
+describe('Subtraction betweeen two numbers', () => {
+  it('should sub 2 and 2 and the result must be 0', () => {
+    expect(sub(2, 2)).toBe(0);
+  });
+
+  it('should sub 2 and 2 even if one of them is a string and the result must be 0', () => {
+    expect(sub('2', '2')).toBe(0);
+  });
+
+  it('should throw an error if what is provided to the method cannot be subtracted', () => {
     expect(() => {
-        sum('', 2)
+      sub('', 2);
     }).toThrowError();
 
     expect(() => {
-      sum([2, 2])
+      sub([2, 2]);
     }).toThrowError();
 
     expect(() => {
-        sum({})
+      sub({});
     }).toThrowError();
 
     expect(() => {
-      sum()
+      sub();
     }).toThrowError();
+  });
 });
+
+describe('Multiplication betweeen two numbers', () => {
+  it('should multiply 3 and 3 and the result must be 9', () => {
+    expect(mult(3, 3)).toBe(9);
+  });
+
+  it('should multiply 3 and 3 even if one of them is a string and the result must be 9', () => {
+    expect(mult('3', '3')).toBe(9);
+  });
+
+  it('should throw an error if what is provided to the method cannot be multiplied', () => {
+    expect(() => {
+      mult('', 2);
+    }).toThrowError();
+
+    expect(() => {
+      mult([2, 2]);
+    }).toThrowError();
+
+    expect(() => {
+      mult({});
+    }).toThrowError();
+
+    expect(() => {
+      mult();
+    }).toThrowError();
+  });
+});
+
+describe('Division between two numbers', () => {
+    it('should divide 2 and 2 and the result must be 1', () => {
+      expect(div(2, 2)).toBe(1);
+    });
+})
